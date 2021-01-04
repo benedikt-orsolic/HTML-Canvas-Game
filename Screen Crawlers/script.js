ab82afa387444d068e512eaf4f84f70c67fe0a94
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-01-04 21:08:37 +0100

    Working jump animation

--- Screen Crawlers/script.js
+++ Screen Crawlers/script.js
@@ -1,27 +1,58 @@
+class Crawler {
+    
+    static sptMap = new Image();
+
+    static initClass() {
+        this.sptMap.src = 'images/character.png';
+    }
+
+    constructor (direction) {
+        this.currentFrame = {x:0, y:7};  
+        this.dir = direction;
+        
+        this.frameW = 103.0625;
+        this.frameH = 113.125;
+    }
+
+    drawFrame ( canvasContext, frameX, frameY, img ) {
+
+        canvasContext.drawImage(img, 
+            this.frameW * this.currentFrame.x, this.frameH * this.currentFrame.y, this.frameW, this.frameH, 
+            frameX, frameY, this.frameW*1.5, this.frameH*1.5);
+
+        //Jump
+        this.currentFrame.x++;
+        this.currentFrame.x = this.currentFrame.x < 0 ? 4 : this.currentFrame.x;
+        this.currentFrame.x = this.currentFrame.x > 4 ? 0 : this.currentFrame.x;
+    }
+}
+
+
+
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-let crawlerSprite = new Image();
-crawlerSprite.src = 'images/character.png';
-let frameWidth = 103.0625;
-let frameHeight = 113.125;
-let currentFrame = {x:0, y:7};
+Crawler.initClass();
+let c = new Crawler( 0 );
 
-context.fillRect( 50, 50, 100, 100);
+let sptMap = new Image();
+sptMap.src = 'images/character.png';
 
-crawlerSprite.onload = function() {
+sptMap.onload = function() {
     window.onload = setInterval(animate, 100);
 }
 
+
+
 function animate(){
     context.clearRect(0, 0, canvas.height, canvas.width);
-    context.drawImage(crawlerSprite, frameWidth*currentFrame.x, frameHeight*currentFrame.y, frameWidth, frameHeight, 25, 25, frameWidth*2, frameHeight*2);
-    currentFrame.x++;
-    if( currentFrame.x > 4) currentFrame.x = 0;
+    c.drawFrame( context, 50, 50, sptMap);
 }
 //ctx.drawImage(crawlerSprite, sX, sY, sW, sH, dX, dY, dW, dH)
 
 //window.onload = setInterval(animate, 1000/39);
+
+
